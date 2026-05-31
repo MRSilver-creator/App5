b4f1a76c1feafbb489d5e0bb94d933607b6e190d
MRSilver-creator <wassimsaad485@gmail.com>
2026-05-31 13:58:32 +0400

    second commit

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -2,16 +2,16 @@ export const environment = {
   production: false,
 
   firebaseConfig: {
-    apiKey:            'AIzaSyDgC4C6zd28Xhx8wT0dMtktGFrdhzUEqH4',
-    authDomain:        'vitaltrend-e78a1.firebaseapp.com',
-    databaseURL:       'https://vitaltrend-e78a1-default-rtdb.europe-west1.firebasedatabase.app',
-    projectId:         'vitaltrend-e78a1',
-    storageBucket:     'vitaltrend-e78a1.appspot.com',
-    messagingSenderId: '1083818335122',
-    appId:             '1:1083818335122:web:472a2907470ab7f7ba3f4b',
+    apiKey:            '',
+    authDomain:        '',
+    databaseURL:       '',
+    projectId:         '',
+    storageBucket:     '',
+    messagingSenderId: '',
+    appId:             '',
   },
 
-  googleMapsApiKey: 'AIzaSyBsoY-oxOyg1qcv3Oq5u9bpmrOUl2U0ZuQ',
+  googleMapsApiKey: '',
 
-  geminiApiKey: 'AIzaSyBVL4YlqJDSfwV5Zv8ZuKRrEsBeAqmV4eg',
+  geminiApiKey: '',
 };
