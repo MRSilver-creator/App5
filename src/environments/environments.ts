3b06f1dc96281dae1b7dd482c74426aed16e34c3
MRSilver-creator <wassimsaad485@gmail.com>
2026-06-19 20:40:17 +0400

    first commit

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -2,14 +2,14 @@ export const environment = {
   production: false,
 
   firebaseConfig: {
-    apiKey:            '',
-    authDomain:        '',
-    databaseURL:       '',
-    projectId:         '',
-    storageBucket:     '',
-    messagingSenderId: '',
-    appId:             '',
+    apiKey:            'AIzaSyDgC4C6zd28Xhx8wT0dMtktGFrdhzUEqH4',
+    authDomain:        'vitaltrend-e78a1.firebaseapp.com',
+    databaseURL:       'https://vitaltrend-e78a1-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId:         'vitaltrend-e78a1',
+    storageBucket:     'vitaltrend-e78a1.appspot.com',
+    messagingSenderId: '1083818335122',
+    appId:             '1:1083818335122:web:472a2907470ab7f7ba3f4b',
   },
 
-  geminiApiKey: '',
+  geminiApiKey: 'AQ.Ab8RN6J5WQR-5IipLbbR3dhBdZOBJFlcJLsOvbS7vYbXglM3mw',
 };
